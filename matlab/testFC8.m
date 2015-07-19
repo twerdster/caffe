3ba5c98ce8ddeabe4eb486e4fe912408d6423b9e
twerdster <twerdster@gmail.com>
2015-07-20 00:44:05 +0300

    Added sample data

--- matlab/testFC8.m
+++ matlab/testFC8.m
@@ -4,26 +4,36 @@ caffe.reset_all()
 avg = cat(3,122.67891434,116.66876762,104.00698793);
 
 net = caffe.Net('/home/gipadmin/caffe_fc/models/fc8/fcn-8s-pascal-deploy.prototxt','/home/gipadmin/caffe_fc/models/fc8/fcn-8s-pascal.caffemodel','test');
+vidFile = 'object.mp4';
+vid = VideoReader(vidFile);
 
-I=imread('/home/gipadmin/Downloads/people.jpg');
+%I=imread('people.jpg');
 
-subplot(121)
-I = imresize(I,[500 500]); I_=I;
-imshow(I_)
-
-I = single(I) - repmat(avg,[500 500]);
-I = I(:,:,[3 2 1]);
-I = permute(I,[2 1 3]);
-
-net.blobs('data').set_data(reshape(I,[500 500 3 1]));
-
-net.forward_prefilled();
-
-[m,ind]=max(net.blobs('upscore').get_data(),[],3);
-
-subplot(122)
-
-imagesc(ind')
-axis equal;
-axis tight;
-axis off;
+for i=1:196
+    I = read(vid,i);
+    
+    subplot(121)
+    I = imresize(I,[500 500]); I_=I;
+    imshow(I_)
+    
+    I = single(I) - repmat(avg,[500 500]);
+    I = I(:,:,[3 2 1]);
+    I = permute(I,[2 1 3]);
+    
+    net.blobs('data').set_data(reshape(I,[500 500 3 1]));
+    
+    net.forward_prefilled();
+    
+    [m,ind]=max(net.blobs('upscore').get_data(),[],3);
+    
+    subplot(122)
+    
+    imagesc(ind')
+    axis equal;
+    axis tight;
+    axis off;
+    imwrite(uint16(ind'),sprintf('outImages/TAG%.4i.tiff',i));
+    imwrite(I_,sprintf('outImages/IM%.4i.png',i));
+    
+    drawnow
+end
